bf26b102419efde3adc01f1b7d94b7dbf0965d7e
jvalencial31 <jvalenciaar@unal.edu.co>
2024-02-27 22:15:20 -0500

    jose

--- clean_data.py
+++ clean_data.py
@@ -5,76 +5,66 @@ import pandas as pd
 
 def load_data(input_file):
     """Lea el archivo usando pandas y devuelva un DataFrame"""
-    data=pd.read_csv(input_file,sep='\t')
-    return data
+
+    #
+    # Esta parte es igual al taller anterior
+    #
+    df = pd.read_csv(input_file)
+    return df
 
 
 def create_key(df, n):
     """Cree una nueva columna en el DataFrame que contenga el key de la columna 'text'"""
 
     df = df.copy()
+    df["key"] = df["text"]
+    df["key"] = df["key"].str.strip()
+    df["key"] = df["key"].str.lower()
+    df["key"] = df["key"].str.replace("-", "")
+    df["key"] = df["key"].str.translate(
+        str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
+    )
+    df["key"] = df["key"].str.split()
 
-    # 1. Copie la columna 'text' a la columna 'fingerprint'
-    df['fingerprint']=df['text']
-    # 2. Remueva los espacios en blanco al principio y al final de la cadena
-    df['fingerprint']=df['fingerprint'].str.strip()
-    # 3. Convierta el texto a minúsculas
-    df['fingerprint']=df['fingerprint'].str.lower()
-    # 4. Transforme palabras que pueden (o no) contener guiones por su version sin guion.
-    df['fingerprint']=df['fingerprint'].str.replace('-','')
-    # 5. Remueva puntuación y caracteres de control
-    df['fingerprint']=df['fingerprint'].str.replace('.','')
-    # 6. Convierta el texto a una lista de tokens
-    df['fingerprint']=df['fingerprint'].str.split()
-    # Una el texto sin espacios en blanco
-    df['fingerprint']=df['fingerprint'].str.join('')
-    # Convierta el texto a una lista de n-gramas
-    df['fingerprint']=df['fingerprint'].apply(lambda x: [x[i:i+n] for i in range(len(x)-n+1)])
-    # 8. Ordene la lista de tokens y remueve duplicados
-    df['fingerprint']=df['fingerprint'].apply(lambda x: sorted(set(x)))  
-    # 9. Convierta la lista de tokens a una cadena de texto separada por espacios
-    df['fingerprint']=df['fingerprint'].str.join(' ')
+    # ------------------------------------------------------
+    # Esta es la parte especifica del algoritmo de n-gram:
+    #
+    # - Una el texto sin espacios en blanco
+    df["key"] = df["key"].str.join("")
+    #
+    # - Convierta el texto a una lista de n-gramas
+    df["key"] = df["key"].map(lambda x: [x[t : t + n - 1] for t in range(len(x))])
+    #
+    # - Ordene la lista de n-gramas y remueve duplicados
+    df["key"] = df["key"].apply(lambda x: sorted(set(x)))
+    #
+    # - Convierta la lista de ngramas a una cadena
+    df["key"] = df["key"].str.join("")
+    ## ------------------------------------------------------
 
-    # Copie la columna 'text' a la columna 'key'
-    # Remueva los espacios en blanco al principio y al final de la cadena
-    # Convierta el texto a minúsculas
-    # Transforme palabras que pueden (o no) contener guiones por su version sin guion.
-    # Remueva puntuación y caracteres de control
-    # Convierta el texto a una lista de tokens
-    
-    
-    # Ordene la lista de n-gramas y remueve duplicados
-    # Convierta la lista de ngramas a una cadena
-    
     return df
 
 
 def generate_cleaned_column(df):
     """Crea la columna 'cleaned' en el DataFrame"""
 
+    #
+    # Este código es identico al anteior
+    #
     df = df.copy()
-
-    # 1. Ordene el dataframe por 'fingerprint' y 'text'
-    df=df.sort_values(by=['fingerprint','text']).copy()
-    # 2. Seleccione la primera fila de cada grupo de 'fingerprint'
-    fingerprints=df.groupby('fingerprint').first().reset_index()
-    # 3.  Cree un diccionario con 'fingerprint' como clave y 'text' como valor
-    fingerprints=fingerprints.set_index('fingerprint')['text'].to_dict()
-    # 4. Cree la columna 'cleaned' usando el diccionario
-    df['cleaned']=df['fingerprint'].map(fingerprints)
-    return df
-
-    # Ordene el dataframe por 'key' y 'text'
-    # Seleccione la primera fila de cada grupo de 'key'
-    # Cree un diccionario con 'key' como clave y 'text' como valor
-    # Cree la columna 'cleaned' usando el diccionario
+    df = df.sort_values(by=["key", "text"], ascending=[True, True])
+    keys = df.drop_duplicates(subset="key", keep="first")
+    key_dict = dict(zip(keys["key"], keys["text"]))
+    df["cleaned"] = df["key"].map(key_dict)
 
     return df
 
 
 def save_data(df, output_file):
     """Guarda el DataFrame en un archivo"""
-
+    #
+    # Este código es identico al anteior
+    #
     df = df.copy()
     df = df[["cleaned"]]
     df = df.rename(columns={"cleaned": "text"})
@@ -83,7 +73,9 @@ def save_data(df, output_file):
 
 def main(input_file, output_file, n=2):
     """Ejecuta la limpieza de datos"""
-
+    #
+    # Este código es identico al anteior
+    #
     df = load_data(input_file)
     df = create_key(df, n)
     df = generate_cleaned_column(df)
